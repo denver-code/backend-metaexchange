3b07285b99b04bfcb5e221195969771044a9c8a2
Denver <csigorek@gmail.com>
2021-11-17 20:10:27 +0200

    update signup - added ratelimiter - added login_required filter - email regex - password regex (hash scanner)

--- app/v1/public/authorization/signup.py
+++ app/v1/public/authorization/signup.py
@@ -1,7 +1,14 @@
 from fastapi import (
     APIRouter,
+    Depends
 )
-from pydantic import BaseModel
+from fastapi_limiter.depends import RateLimiter
+from pydantic import (
+    BaseModel,
+    validator
+)
+import re
+from app.v1.public.authorization.methods import *
 
 signup = APIRouter(prefix="/signup")
 
@@ -12,7 +19,21 @@ class User(BaseModel):
     wallets: list
     history: list
 
+    @validator("email")
+    def email_regex(cls, v):
+        regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+        if not re.fullmatch(regex, v):
+            raise ValueError("Invalid email")
+        return v
 
-@signup.post("/")
+    @validator("password")
+    def password_hash_checker(cls, v):
+        regex = r"^[a-fA-F0-9]{64}$"
+        if not re.fullmatch(regex, v):
+            raise ValueError("Need hashed password")
+        return v
+
+
+@signup.post("/", dependencies=[Depends(login_required), Depends(RateLimiter(times=1, seconds=5))])
 async def signup_event(user: User):
     return user
