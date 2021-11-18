45bcf70097ccc700054cfcbcbe1f4ebc7bc448d0
Denver <csigorek@gmail.com>
2021-11-18 21:17:41 +0200

    simple login_required

--- app/v1/public/authorization/methods.py
+++ app/v1/public/authorization/methods.py
@@ -2,17 +2,21 @@ from fastapi import (
     HTTPException,
     Header
 )
-
-import jwt
+from app.v1.api.database_user_api import get_user
 import os
+import jwt
 import hashlib
 
 
 async def login_required(Authorization=Header("Authorization")):
     try:
-        session = Authorization
+        _jwt = jwt.decode(Authorization, os.getenv('SECRET_JWT', "YOURTOKENHERE"), algorithm="HS256")
+        user_object = await get_user(_jwt["email"])
+        if _jwt["session"] not in user_object["sessions"]:
+            raise HTTPException(status_code=401, detail="Unauthorized")
     except:
         raise HTTPException(status_code=401, detail="Unauthorized")
+
 
 async def get_password_hash(password):
     return hashlib.sha256(password.encode('utf-8')).hexdigest()
