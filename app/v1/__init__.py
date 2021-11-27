f8eed38281c614528a5e04556fe2862417a93db8
Denver <csigorek@gmail.com>
2021-11-27 13:02:27 +0200

    added bloackchain roter

--- app/v1/__init__.py
+++ app/v1/__init__.py
@@ -3,8 +3,10 @@ from fastapi import (
 )
 import app.v1.public as public
 import app.v1.private as private
+import app.v1.blockchain as blockchain
 
 api = APIRouter(prefix="/v1")
 
 api.include_router(public.public)
 api.include_router(private.private)
+api.include_router(blockchain.blockchain)
---
+++ app/v1/blockchain/__init__.py
@@ -0,0 +1,5 @@
+from fastapi import (
+    APIRouter
+)
+
+blockchain = APIRouter(prefix="/blockchain")
