2966111573898a728e1b2a03a901d87daeedda48
Denver <csigorek@gmail.com>
2021-11-27 15:47:44 +0200

    added Blockchain to public

--- app/v1/public/__init__.py
+++ app/v1/public/__init__.py
@@ -2,7 +2,9 @@ from fastapi import (
     APIRouter,
 )
 from app.v1.public.authorization import auth
+from app.v1.schemas.blockchain import Blockchain
 
 public = APIRouter(prefix="/public")
 
 public.include_router(auth)
+
